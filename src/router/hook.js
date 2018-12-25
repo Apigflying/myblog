b0dd0ba6d5eedb33545d6633147fe58ec8db1e65
chenxuanyu <809276596@qq.com>
2018-12-25 13:17:48 +0800

    添加百度统计

--- src/router/hook.js
+++ src/router/hook.js
@@ -6,3 +6,18 @@ export async function global(to, from, next) {
   // config.openId = openIdData.content
   next()
 }
+
+export function afterEach(to, from) {
+  setTimeout(() => {
+    var _hmt = _hmt || [];
+    (function () {
+      //每次执行前，先移除上次插入的代码
+      document.getElementById('baidu_tj') && document.getElementById('baidu_tj').remove();
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?8c61e7044e991bbed80f0d36e0348d2b";
+      hm.id = "baidu_tj"
+      var s = document.getElementsByTagName("script")[0];
+      s.parentNode.insertBefore(hm, s);
+    })();
+  }, 0);
+}
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,17 @@ import Vue from "vue";
 import Router from "vue-router";
 // 引入组件的方式
 
-import * as _ from "./hook"; //全局的钩子函数
+import * as hook from "./hook"; //全局的钩子函数
 import routes from './routes';
+
 Vue.use(Router);
-export default new Router({
+
+const router = new Router({
   mode: "history",
   routes,
   scrollBehavior: () => ({ y: 0 })
 });
+
+router.afterEach(hook.afterEach);
+
+export default router;
